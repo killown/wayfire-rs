8a649df68a93a763aa524afc48df3f7bea483748
killown <systemofdown@gmail.com>
2024-08-10 00:02:03 -0300

    Update main.rs

--- examples/move_view_to_workspace/src/main.rs
+++ examples/move_view_to_workspace/src/main.rs
@@ -5,15 +5,12 @@ use wayfire_rs::ipc::WayfireSocket;
 async fn main() -> Result<(), Box<dyn Error>> {
     // Establish a connection to the Wayfire IPC socket.
     let mut socket = WayfireSocket::connect().await?;
-
-    // Retrieve the currently focused view and output.
-    let focused_view = socket.get_focused_view().await?;
-    let focused_output: i64 = focused_view.output_id;
-
+    
     // Extract the ID of the focused view and output.
+    let focused_view = socket.get_focused_view().await?;
     let view_id: i64 = focused_view.id;
-    let output_id: i64 = focused_output;
-
+    let focused_output: i64 = focused_view.output_id;
+    
     // move focused view to workspace 4 considering a workspace grid 3x3
     match socket.set_workspace(1, 1, view_id, output_id).await {
         Ok(_) => println!("Successfully set workspace"),
