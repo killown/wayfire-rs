d9bcedd32b96cb7fafec33498d991038d8c997b7
killown <systemofdown@gmail.com>
2024-08-09 23:56:46 -0300

    Update main.rs

--- examples/move_view_to_workspace/src/main.rs
+++ examples/move_view_to_workspace/src/main.rs
@@ -14,6 +14,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let view_id: i64 = focused_view.id;
     let output_id: i64 = focused_output;
 
+    // move focused view to workspace 4 considering a workspace grid 3x3
     match socket.set_workspace(1, 1, view_id, output_id).await {
         Ok(_) => println!("Successfully set workspace"),
         Err(e) => eprintln!("Failed to set workspace: {}", e),
